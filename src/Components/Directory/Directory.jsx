46e1b881ac472363448bf5217edea0cd32ff3e08
mAmineSouissi <mohamedamine.souissi@esprit.tn>
2024-08-06 03:36:32 +0100

    initial commit : feature : added Components set the layout of the home page to the project

---
+++ src/Components/Directory/Directory.jsx
@@ -0,0 +1,11 @@
+import CategoryItem from "../Category-item/CategoryItem";
+
+const Directory = ({categories}) => {
+    return (<div className='categories-container'>
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>);
+}
+
+export default Directory;
